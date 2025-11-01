c568b7eb8434b7f8acb2a87921718c2785e242ac
Sebastian <contreras.sebastian.dev@gmail.com>
2025-10-31 23:37:17 -0600

    feat: implement Process page with data fetching and rendering

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,29 +1,41 @@
-import { z } from 'astro:content'
+import { z } from "astro:content"
 
 const imageSchema = z.object({
-    url: z.string(),
-    width: z.number(),
-    height: z.number()
+	url: z.string(),
+	width: z.number(),
+	height: z.number()
 })
 
 const featuredImagesSchema = z.object({
-    thumbnail: imageSchema,
-    medium: imageSchema,
-    medium_large: imageSchema,
-    large: imageSchema,
-    full: imageSchema
+	thumbnail: imageSchema,
+	medium: imageSchema,
+	medium_large: imageSchema,
+	large: imageSchema,
+	full: imageSchema
 })
 
 export const BaseWPSchema = z.object({
-    id: z.number(),
-    title: z.object ({
-        rendered: z.string()
-    }),
-    content: z.object ({
-        rendered: z.string()
-    }),
-    featured_images: featuredImagesSchema,
+	id: z.number(),
+	title: z.object({
+		rendered: z.string()
+	}),
+	content: z.object({
+		rendered: z.string()
+	}),
+	featured_images: featuredImagesSchema,
+	acf: z.object({
+		subtitle: z.string()
+	})
+})
+
+const processSchema = z.object({
+	title: z.string(),
+	description: z.string(),
+	image: z.string()
+})
+
+export const ProcessPageSchema = BaseWPSchema.extend({
     acf: z.object({
-        subtitle: z.string()
-    })
+        subtitle: z.string(),
+    }).catchall(processSchema)
 })
